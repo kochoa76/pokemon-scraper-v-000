169928c0237a28b13806427e88c532408833c44f
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-29 18:31:35 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,5 +19,10 @@ def self.find(id, db)
     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
 end
 
-def alter_hp(pokemon)
+def alter_hp(new_hp, db)
+  
+  
+  
 end
+
+end 
