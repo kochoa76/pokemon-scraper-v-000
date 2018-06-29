0071ef203dc68325c4d3a816e8e9f1512ebb853b
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-29 18:01:46 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,11 +10,10 @@ def self.save(name, type, db)
   db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
 end
 
-  def self.find(id, db)
-    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
-    self.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+def self.find(id_num, db)
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
   end
-
   def self.all
     @@all
   end
