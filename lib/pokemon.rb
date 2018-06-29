56295add5976a81ca4c5f492428b45e393d2f71c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-29 18:03:43 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,10 @@ attr_accessor :id, :name, :type, :db
 
 
 def initialize(id:, name:, type:, hp: nil, db:)
-  @id, @name, @type, @hp, @db = id, name, type, hp, db
+  @id = id
+  @name = name
+  @type = type
+  @db = db
 end
 
 def self.save(name, type, db)
